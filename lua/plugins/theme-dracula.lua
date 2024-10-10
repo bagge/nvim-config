5d943ef79abfe2b46525be6b4a8981612415bb1e
bagge <alexander.szakaly@gmail.com>
2024-10-10 21:07:57 +0200

    Add window-picker overrides

--- lua/plugins/theme-dracula.lua
+++ lua/plugins/theme-dracula.lua
@@ -5,7 +5,9 @@ return {
     require("dracula").setup({
       overrides = {
         -- Change the bg of the statusline not have the same bg as buffer
-        StatusLineNC = { fg = colors.comment, bg = colors.menu }
+        StatusLineNC = { fg = colors.comment, bg = colors.menu },
+        WindowPickerStatusLine = { fg = colors.white, bg = colors.comment },
+        WindowPickerStatusLineNC = { fg = colors.white, bg = colors.comment },
       },
     })
   end,
