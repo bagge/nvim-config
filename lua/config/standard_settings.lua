c4b04f70594bc2ca3cf0aee624ee4abbbf91274a
bagge <alexander.szakaly@gmail.com>
2024-10-14 09:17:24 +0200

    Add short commands for fzf-lua
    As a way to avoid cluttering of short leader based keymaps. Keeping the
    existing keymaps until I have decided which is nicer.

--- lua/config/standard_settings.lua
+++ lua/config/standard_settings.lua
@@ -21,3 +21,14 @@ vim.cmd[[imap jj <Esc>]]
 -- Leader key
 vim.keymap.set('n', '<Space>', '', {})
 vim.g.mapleader = " "
+
+-- Fzf-lua shorthand commands, as an alternative to leader keymaps
+vim.api.nvim_create_user_command(
+  "B",
+  function() require('fzf-lua').buffers() end,
+  { desc = "Fzf buffers" })
+
+vim.api.nvim_create_user_command(
+  "F",
+  function() require('fzf-lua').files() end,
+  { desc = "Fzf files" })
