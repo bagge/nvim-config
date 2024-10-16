be0fcae6215b4b44c54ab78cc4f2c313f9cfe7dd
bagge <alexander.szakaly@gmail.com>
2024-10-16 09:45:43 +0200

    Add nvim-treesitter plugin

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+		require("nvim-treesitter.configs").setup {
+			highlight = {
+				enable = true,
+			},
+			ensure_installed = {
+				"vimdoc",
+				"luadoc",
+				"vim",
+				"lua",
+				"markdown"
+			}
+		}
+	end,
+}
