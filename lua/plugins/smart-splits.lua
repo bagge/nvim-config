e09007382d31852f9b3ae892c9b9cd3be878fad8
bagge <alexander.szakaly@gmail.com>
2024-10-16 10:39:10 +0200

    Make smartsplit movement silent

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -1,19 +1,23 @@
+silent_movement = function(direction)
+  return vim.cmd("silent! SmartCursorMove" .. direction)
+end
+
 return {
   "mrjones2014/smart-splits.nvim",
   lazy = false,
   build = "./kitty/install-kittens.bash",
   keys = {
     { "<A-h>", ":SmartResizeLeft<cr>", desc = "Resize left", noremap = true },
-    { "<A-j>", ":SmartResizeDown<cr>", desc = "Resize left", noremap = true },
-    { "<A-k>", ":SmartResizeUp<cr>", desc = "Resize left", noremap = true },
-    { "<A-l>", ":SmartResizeRight<cr>", desc = "Resize left", noremap = true },
-    { "<C-h>", ":SmartCursorMoveLeft<cr>", desc = "Move cursor left", noremap = true },
-    { "<C-j>", ":SmartCursorMoveDown<cr>", desc = "Move cursor left", noremap = true },
-    { "<C-k>", ":SmartCursorMoveUp<cr>", desc = "Move cursor left", noremap = true },
-    { "<C-l>", ":SmartCursorMoveRight<cr>", desc = "Move cursor left", noremap = true },
+    { "<A-j>", ":SmartResizeDown<cr>", desc = "Resize down", noremap = true },
+    { "<A-k>", ":SmartResizeUp<cr>", desc = "Resize up", noremap = true },
+    { "<A-l>", ":SmartResizeRight<cr>", desc = "Resize right", noremap = true },
+    { "<C-h>", function() silent_movement("Left") end, desc = "Move cursor left", noremap = true },
+    { "<C-j>", function() silent_movement("Down") end, desc = "Move cursor down", noremap = true },
+    { "<C-k>", function() silent_movement("Up") end, desc = "Move cursor up", noremap = true },
+    { "<C-l>", function() silent_movement("Right") end, desc = "Move cursor right", noremap = true },
     { "<leader><leader>h", ":SmartSwapLeft<cr>", desc = "Swap buffer left", noremap = true },
-    { "<leader><leader>j", ":SmartSwapDown<cr>", desc = "Swap buffer left", noremap = true },
-    { "<leader><leader>k", ":SmartSwapUp<cr>", desc = "Swap buffer left", noremap = true },
-    { "<leader><leader>l", ":SmartSwapRight<cr>", desc = "Swap buffer left", noremap = true },
+    { "<leader><leader>j", ":SmartSwapDown<cr>", desc = "Swap buffer down", noremap = true },
+    { "<leader><leader>k", ":SmartSwapUp<cr>", desc = "Swap buffer up", noremap = true },
+    { "<leader><leader>l", ":SmartSwapRight<cr>", desc = "Swap buffer right", noremap = true },
   },
 }
