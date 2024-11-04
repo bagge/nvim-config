aeb30e16596de46af1c59dd2eab756d77b83648f
bagge <alexander.szakaly@gmail.com>
2024-11-04 09:28:26 +0100

    Add keymaps for FzfLua live grep

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -5,6 +5,9 @@ return {
   keys = {
     { "<leader>b", ":FzfLua buffers<cr>", desc = "Fuzzy search buffers", noremap = true },
     { "<leader>f", ":FzfLua files<cr>", desc = "Fuzzy search files", noremap = true },
+    { "<leader>ll", ":FzfLua live_grep<cr>", desc = "Live grep", noremap = true },
+    { "<leader>lg", ":FzfLua live_grep_glob<cr>", desc = "Live grep with glob", noremap = true },
+    { "<leader>lr", ":FzfLua live_grep<cr>", desc = "Resume live grep", noremap = true },
   },
   cmd = {
     "FzfLua",
