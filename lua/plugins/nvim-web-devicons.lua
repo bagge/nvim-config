a8ea3b8d87a81b7ef5004c41dddf4ea2e5ac5d44
bagge <alexander.szakaly@gmail.com>
2026-05-15 09:49:35 +0200

    Add overrides for missing yaml glyphs
    It seems that the updated glyphs are not yet included in the Hack Nerd
    Font currently used. Could maybe be removed later.

---
+++ lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-tree/nvim-web-devicons",
+  opts = {
+    override_by_extension = {
+      yaml = {
+        icon = "",
+        color = "#6D8086",
+        cterm_color = "66",
+        name = "Yaml",
+      },
+      yml = {
+        icon = "",
+        color = "#6D8086",
+        cterm_color = "66",
+        name = "Yml",
+      },
+    },
+  },
+}
